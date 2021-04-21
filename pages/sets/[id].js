965e429501f381f13b6114b5c245b302c9b41c43
Thanayut T <30551284+tithanayut@users.noreply.github.com>
2021-04-21 20:10:48 +0700

    Create card component

---
+++ pages/sets/[id].js
@@ -0,0 +1,134 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+const SetPage = () => {
+	return (
+		<Fragment>
+			<Head>
+				<title>Flashcards</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<div className="w-2/3 mt-8 mx-auto ">
+				<Link href="/collection">
+					<span className="flex items-center text-gray-600 cursor-pointer">
+						<svg
+							className="w-4 h-4 mr-1"
+							fill="none"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M15 19l-7-7 7-7"
+							/>
+						</svg>
+						Back to Collection
+					</span>
+				</Link>
+
+				<div className="flex justify-between">
+					<div>
+						<p className="text-2xl mt-6 text-green-600 font-bold">
+							Thai Level I
+						</p>
+						<p className="text-gray-600 font-semibold mt-2">
+							Learn Thai from the beginning. Starts with simple
+							words.
+						</p>
+					</div>
+					<Link href="/collection">
+						<span className="flex items-center text-green-600 cursor-pointer">
+							Edit
+							<svg
+								className="w-4 h-4 ml-2"
+								fill="none"
+								stroke="currentColor"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
+									d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+								/>
+							</svg>
+						</span>
+					</Link>
+				</div>
+				<div className="flex justify-around items-center">
+					<div className="flex justify-end w-1/4 cursor-pointer">
+						<svg
+							className="w-6 h-6 mr-4"
+							fill="none"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
+							/>
+						</svg>
+					</div>
+					<div className="flex justify-center w-1/2">
+						<div
+							className="flex justify-center items-center w-96 mt-8 bg-green-100 rounded-xl"
+							style={{ minHeight: "300px" }}
+						>
+							<p className="text-xl text-gray-600 font-semibold p-6">
+								lorem lorem lorem lorem lorem lorem lorem
+								loremlorem lorem lorem loremlorem lorem lorem
+								loremlorem lorem lorem loremlorem lorem lorem
+								loremlorem lorem lorem loremlorem lorem lorem
+								lorem
+							</p>
+						</div>
+					</div>
+					<div className="flex justify-start w-1/4 cursor-pointer">
+						<svg
+							className="w-6 h-6 ml-4"
+							fill="none"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+					</div>
+				</div>
+				<div className="flex justify-center items-center text-gray-600 mt-6 font-semibold cursor-pointer">
+					Flip
+					<svg
+						className="w-5 h-5 ml-2"
+						fill="none"
+						stroke="currentColor"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth={2}
+							d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+						/>
+					</svg>
+				</div>
+			</div>
+		</Fragment>
+	);
+};
+
+export default SetPage;
