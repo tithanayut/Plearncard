5863f303e699d9ee62b76886250e10b86cd6d593
Thanayut T <30551284+tithanayut@users.noreply.github.com>
2021-04-21 19:33:54 +0700

    Improve css styles

--- pages/collection.js
+++ pages/collection.js
@@ -2,7 +2,6 @@ import { Fragment } from "react";
 import Head from "next/head";
 import Link from "next/link";
 
-import Profile from "../components/Profile";
 import Sets from "../components/Sets/Sets";
 
 const CollectionPage = () => {
@@ -13,10 +12,11 @@ const CollectionPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<Profile username="bankbkbkk" />
 			<div className="w-2/3 mt-8 mx-auto">
 				<div className="flex justify-between items-center">
-					<p>You have 3 sets of cards in your collection.</p>
+					<p className="text-gray-600">
+						You have 3 sets of cards in your collection.
+					</p>
 					<Link href="/create">
 						<span className="flex justify-center items-center w-24 h-10 bg-green-200 text-gray-600 rounded-lg cursor-pointer hover:bg-green-300 hover:shadow-sm">
 							Create
