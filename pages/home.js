f5a7efe2422546b09cdb4adf62882d5bd67562a7
Thanayut T <30551284+tithanayut@users.noreply.github.com>
2021-04-21 19:32:36 +0700

    Create profile + home page

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,8 @@
 import { Fragment } from "react";
 import Head from "next/head";
+import Link from "next/link";
+
+import Sets from "../components/Sets/Sets";
 
 const HomePage = () => {
 	return (
@@ -8,6 +11,43 @@ const HomePage = () => {
 				<title>Flashcards</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+
+			<div className="w-2/3 mt-8 mx-auto">
+				<div className="flex justify-between items-center">
+					<div>
+						<h1 className="text-2xl text-green-600 font-bold">
+							Welcome to Flashcards!
+						</h1>
+						<p class="text-gray-600 mt-2">
+							Below is your recent sets. Or click create to build
+							new one.
+						</p>
+					</div>
+					<Link href="/create">
+						<span className="flex justify-center items-center w-24 h-10 bg-green-200 text-gray-600 rounded-lg cursor-pointer hover:bg-green-300 hover:shadow-sm">
+							Create
+							<svg
+								className="w-5 h-5 ml-1"
+								fill="none"
+								stroke="currentColor"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
+									d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+								></path>
+							</svg>
+						</span>
+					</Link>
+				</div>
+				<div className="mt-6">
+					<p className="text-gray-600 font-bold">Recent</p>
+					<Sets />
+				</div>
+			</div>
 		</Fragment>
 	);
 };
---
+++ pages/profile.js
@@ -0,0 +1,123 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+import Profile from "../components/Profile";
+
+const ProfilePage = () => {
+	return (
+		<Fragment>
+			<Head>
+				<title>Flashcards</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<Profile username="bankbkbkk" />
+			<div className="w-2/3 mt-8 mx-auto text-gray-600">
+				<div className="flex justify-between items-center">
+					<p>Joined since 14 May 2020</p>
+					<div className="flex">
+						<Link href="/collection">
+							<span className="flex justify-center items-center w-40 h-10 bg-green-200 text-gray-600 rounded-lg cursor-pointer hover:bg-green-300 hover:shadow-sm">
+								View Collection
+								<svg
+									className="w-5 h-5 ml-2"
+									fill="none"
+									stroke="currentColor"
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth={2}
+										d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+									/>
+								</svg>
+							</span>
+						</Link>
+						<Link href="/create">
+							<span className="flex justify-center items-center ml-2 w-24 h-10 bg-green-200 text-gray-600 rounded-lg cursor-pointer hover:bg-green-300 hover:shadow-sm">
+								Create
+								<svg
+									className="w-5 h-5 ml-1"
+									fill="none"
+									stroke="currentColor"
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth={2}
+										d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+									></path>
+								</svg>
+							</span>
+						</Link>
+					</div>
+				</div>
+				<div className="mt-4">
+					<h2 className="text-2xl font-bold text-green-600">
+						Account Settings
+					</h2>
+					<ul className="flex flex-col mt-2 font-semibold">
+						<li className="flex items-center my-1 cursor-pointer">
+							<svg
+								className="w-4 h-4 mr-1"
+								fill="none"
+								stroke="currentColor"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
+									d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14"
+								/>
+							</svg>
+							Change username
+						</li>
+						<li className="flex items-center my-1 cursor-pointer">
+							<svg
+								className="w-4 h-4 mr-1"
+								fill="none"
+								stroke="currentColor"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
+									d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14"
+								/>
+							</svg>
+							Change password
+						</li>
+						<li className="flex items-center my-1 text-red-600 cursor-pointer">
+							<svg
+								className="w-4 h-4 mr-1"
+								fill="none"
+								stroke="currentColor"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
+									d="M6 18L18 6M6 6l12 12"
+								/>
+							</svg>
+							Delete account
+						</li>
+					</ul>
+				</div>
+			</div>
+		</Fragment>
+	);
+};
+
+export default ProfilePage;
