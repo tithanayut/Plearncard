861ea8710f52a5cd144bfa0fedd8d2487f8f1cdb
Thanayut T <b@thanayut.in.th>
2021-04-27 21:24:52 +0700

    Remove change username and password options
    Since it is handle by OAuth already.

--- pages/profile.js
+++ pages/profile.js
@@ -75,40 +75,6 @@ const ProfilePage = () => {
 						Account Settings
 					</h2>
 					<ul className="flex flex-col mt-2 font-semibold">
-						<li className="flex items-center my-1 cursor-pointer">
-							<svg
-								className="w-4 h-4 mr-1"
-								fill="none"
-								stroke="currentColor"
-								viewBox="0 0 24 24"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth={2}
-									d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14"
-								/>
-							</svg>
-							Change username
-						</li>
-						<li className="flex items-center my-1 cursor-pointer">
-							<svg
-								className="w-4 h-4 mr-1"
-								fill="none"
-								stroke="currentColor"
-								viewBox="0 0 24 24"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth={2}
-									d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14"
-								/>
-							</svg>
-							Change password
-						</li>
 						<li className="flex items-center my-1 text-red-600 cursor-pointer">
 							<svg
 								className="w-4 h-4 mr-1"
