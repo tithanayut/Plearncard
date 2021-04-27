e4ded48075a8804096db6317391fe8cad1bb600a
Thanayut T <b@thanayut.in.th>
2021-04-27 22:03:10 +0700

    Improve navbar UX

--- components/NavBar.js
+++ components/NavBar.js
@@ -47,22 +47,24 @@ const NavBar = () => {
 			<ul className="flex">
 				{session && (
 					<li className="flex items-center mr-4 cursor-pointer">
-						{session.user.name}
 						<Link href="/profile">
-							<svg
-								className="w-6 h-6 ml-2"
-								fill="none"
-								stroke="currentColor"
-								viewBox="0 0 24 24"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-								></path>
-							</svg>
+							<span className="flex items-center">
+								{session.user.name}
+								<svg
+									className="w-6 h-6 ml-2"
+									fill="none"
+									stroke="currentColor"
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+									></path>
+								</svg>
+							</span>
 						</Link>
 					</li>
 				)}
