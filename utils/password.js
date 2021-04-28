928891733ae8118456422a7f6ddaff3c4b62ed2f
Thanayut T <b@thanayut.in.th>
2021-04-28 18:26:27 +0700

    Remove bcrypt

--- utils/password.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-
-export async function hash(password) {
-	return await bcrypt.hash(password, 12);
-}
-
-export async function validate(plaintext, hashed) {
-	return await bcrypt.compare(plaintext, hashed);
-}
