d9c10e6e2b7cb491bef139ecb9ff8a8ecf4e758e
Thanayut T <b@thanayut.in.th>
2021-04-28 18:34:15 +0700

    Remove unused dep

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,5 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
-import { MongoClient } from "mongodb";
-import { validate } from "../../../utils/password";
-
-const uri = process.env.MONGODB_URI;
 
 export default NextAuth({
 	session: {
