fcb27fd25f92196a32b6203a92708c561a5902b6
Thanayut T <b@thanayut.in.th>
2021-04-30 02:42:02 +0700

    Refactor if condition profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -53,85 +53,6 @@ const ProfilePage = () => {
 		router.replace("/login");
 	}
 
-	let content = <div className="loader">Loading...</div>;
-	if (joinedSince) {
-		content = (
-			<Fragment>
-				<div className="flex justify-between items-center">
-					<p>Joined since {joinedSince ? joinedSince : "..."}</p>
-					<div className="flex">
-						<Link href="/cards">
-							<span className="flex justify-center items-center w-40 h-10 bg-green-200 text-gray-600 rounded-lg cursor-pointer hover:bg-green-300 hover:shadow-sm">
-								View Collection
-								<svg
-									className="w-5 h-5 ml-2"
-									fill="none"
-									stroke="currentColor"
-									viewBox="0 0 24 24"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth={2}
-										d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
-									/>
-								</svg>
-							</span>
-						</Link>
-						<Link href="/create">
-							<span className="flex justify-center items-center ml-2 w-24 h-10 bg-green-200 text-gray-600 rounded-lg cursor-pointer hover:bg-green-300 hover:shadow-sm">
-								Create
-								<svg
-									className="w-5 h-5 ml-1"
-									fill="none"
-									stroke="currentColor"
-									viewBox="0 0 24 24"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth={2}
-										d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-									></path>
-								</svg>
-							</span>
-						</Link>
-					</div>
-				</div>
-				<div className="mt-4">
-					<h2 className="text-2xl font-bold text-green-600">
-						Account Setting
-					</h2>
-					<ul className="flex flex-col mt-2 font-semibold">
-						<li className="flex items-center my-1 text-red-600 cursor-pointer">
-							<Link href="/profile/delete">
-								<span className="flex items-center">
-									<svg
-										className="w-4 h-4 mr-1"
-										fill="none"
-										stroke="currentColor"
-										viewBox="0 0 24 24"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											strokeWidth={2}
-											d="M6 18L18 6M6 6l12 12"
-										/>
-									</svg>
-									Delete account
-								</span>
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</Fragment>
-		);
-	}
-
 	return (
 		<Fragment>
 			<Head>
@@ -145,7 +66,87 @@ const ProfilePage = () => {
 				<Profile username="" />
 			)}
 
-			<div className="w-2/3 mt-8 mx-auto text-gray-600">{content}</div>
+			<div className="w-2/3 mt-8 mx-auto text-gray-600">
+				{joinedSince ? (
+					<Fragment>
+						<div className="flex justify-between items-center">
+							<p>
+								Joined since {joinedSince ? joinedSince : "..."}
+							</p>
+							<div className="flex">
+								<Link href="/cards">
+									<span className="flex justify-center items-center w-40 h-10 bg-green-200 text-gray-600 rounded-lg cursor-pointer hover:bg-green-300 hover:shadow-sm">
+										View Collection
+										<svg
+											className="w-5 h-5 ml-2"
+											fill="none"
+											stroke="currentColor"
+											viewBox="0 0 24 24"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth={2}
+												d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+											/>
+										</svg>
+									</span>
+								</Link>
+								<Link href="/create">
+									<span className="flex justify-center items-center ml-2 w-24 h-10 bg-green-200 text-gray-600 rounded-lg cursor-pointer hover:bg-green-300 hover:shadow-sm">
+										Create
+										<svg
+											className="w-5 h-5 ml-1"
+											fill="none"
+											stroke="currentColor"
+											viewBox="0 0 24 24"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												strokeWidth={2}
+												d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+											></path>
+										</svg>
+									</span>
+								</Link>
+							</div>
+						</div>
+						<div className="mt-4">
+							<h2 className="text-2xl font-bold text-green-600">
+								Account Setting
+							</h2>
+							<ul className="flex flex-col mt-2 font-semibold">
+								<li className="flex items-center my-1 text-red-600 cursor-pointer">
+									<Link href="/profile/delete">
+										<span className="flex items-center">
+											<svg
+												className="w-4 h-4 mr-1"
+												fill="none"
+												stroke="currentColor"
+												viewBox="0 0 24 24"
+												xmlns="http://www.w3.org/2000/svg"
+											>
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													strokeWidth={2}
+													d="M6 18L18 6M6 6l12 12"
+												/>
+											</svg>
+											Delete account
+										</span>
+									</Link>
+								</li>
+							</ul>
+						</div>
+					</Fragment>
+				) : (
+					<div className="loader">Loading...</div>
+				)}
+			</div>
 		</Fragment>
 	);
 };
