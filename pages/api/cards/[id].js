f838a105ffa5ee84a46bb06e562e4241b30d7e42
Thanayut T <b@thanayut.in.th>
2021-05-02 16:40:25 +0700

    Implement PUT in card API

--- pages/api/cards/[id].js
+++ pages/api/cards/[id].js
@@ -57,6 +57,46 @@ export default async (req, res) => {
 		}
 
 		return res.status(200).json(result.value);
+	} else if (req.method === "PUT") {
+		if (
+			!req.body.id ||
+			!req.body.topic ||
+			!req.body.description ||
+			typeof req.body.total !== "number" ||
+			typeof req.body.cards !== "object"
+		) {
+			return res.status(400).json({
+				errors: ["Request body not complete or invalid data supplied"],
+			});
+		}
+
+		let result;
+		const client = new MongoClient(uri);
+		try {
+			await client.connect();
+			result = await client
+				.db("plearncard")
+				.collection("cards")
+				.findOneAndUpdate(
+					{ userId, slug: req.body.id },
+					{
+						$set: {
+							name: req.body.topic,
+							description: req.body.description,
+							total: req.body.total,
+							cards: req.body.cards,
+						},
+					}
+				);
+		} catch {
+			return res
+				.status(500)
+				.json({ errors: ["Database connection failed"] });
+		} finally {
+			await client.close();
+		}
+
+		return res.status(200).json(result.value);
 	}
 
 	return res.status(405).json({ errors: ["Method not allowed"] });
