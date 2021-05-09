d634614f47c1cba8cc6f000c5dcd13706ad7328f
Thanayut T <b@thanayut.in.th>
2021-05-09 22:17:30 +0700

    Fix JSX svg strokeWidth to match pattern

--- components/NavBar.js
+++ components/NavBar.js
@@ -72,7 +72,7 @@ const NavBar = () => {
 										<path
 											strokeLinecap="round"
 											strokeLinejoin="round"
-											strokeWidth="2"
+											strokeWidth={2}
 											d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
 										></path>
 									</svg>
@@ -104,7 +104,7 @@ const NavBar = () => {
 									<path
 										strokeLinecap="round"
 										strokeLinejoin="round"
-										strokeWidth="2"
+										strokeWidth={2}
 										d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
 									></path>
 								</svg>
