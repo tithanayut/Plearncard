de622b5b5008a7889ff7d3a098e1ab2614e715e1
Thanayut T <b@thanayut.in.th>
2021-07-29 21:17:32 +0700

    fix(v2): correct tailwind config for prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
     purge: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
+        "./modules/**/*.{js,ts,jsx,tsx}",
+        "./helpers/**/*.{js,ts,jsx,tsx}",
+        "./icons/**/*.{js,ts,jsx,tsx}",
     ],
     darkMode: false,
     theme: {
