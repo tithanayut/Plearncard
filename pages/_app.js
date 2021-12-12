6be7640fc031bf40a38463080d44ea7157395c98
Thanayut T <b@thanayut.in.th>
2021-12-13 00:20:46 +0700

    feat: install and config nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Provider } from "next-auth/client";
+import NextNProgress from "nextjs-progressbar";
 import NavBar from "../modules/layout/NavBar/NavBar";
 
 import "../styles/globals.css";
@@ -7,6 +8,7 @@ import "../styles/globals.css";
 function App({ Component, pageProps }) {
     return (
         <Provider session={pageProps.session}>
+            <NextNProgress color="#FFBF00" height={4} stopDelayMs={150} />
             <Head>
                 <title>Plearncard</title>
                 <link rel="icon" href="/favicon.svg" />
