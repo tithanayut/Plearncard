7fc8aa01a56ed2da11c5bee995c656aec1edea94
Thanayut T <b@thanayut.in.th>
2021-12-14 22:06:39 +0700

    chore(translate): Staticpages

--- modules/staticpages/IndexPage.js
+++ modules/staticpages/IndexPage.js
@@ -1,25 +1,27 @@
 import Link from "next/link";
+import { useTranslation } from "next-i18next";
 import FlashIcon from "../../icons/FlashIcon";
 import GitHubIcon from "../../icons/GitHubIcon";
 
 const IndexPage = () => {
+    const { t } = useTranslation(["staticpages", "common"]);
+
     return (
         <>
             <div className="flex flex-col justify-center items-center bg-gray-100">
                 <img className="h-24 w-24 mt-14" src="/favicon.svg" />
                 <p className="text-2xl text-green-600 font-bold">
-                    Welcome to Plearncard!
+                    {t("welcome_to_plearncard")}
                 </p>
                 <p className="text-xl text-center text-gray-600 mx-4 mt-2 mb-14">
-                    Plearncard is a web application that helps you learn
-                    anything through flashcards.
+                    {t("plearncard_desc")}
                 </p>
             </div>
             <div className="flex flex-col items-center text-green-600 mt-8">
-                <p className="text-xl font-bold">Are you ready?</p>
+                <p className="text-xl font-bold">{t("are_you_ready")}</p>
                 <Link href="/login">
                     <span className="flex justify-center items-center px-4 h-10 mt-4 font-bold bg-green-200 text-gray-600 rounded-lg cursor-pointer hover:bg-green-300 hover:shadow-sm">
-                        Get Started with Plearncard
+                        {t("get_started")}
                         <FlashIcon className="w-5 h-5 ml-1" />
                     </span>
                 </Link>
@@ -31,7 +33,7 @@ const IndexPage = () => {
                     target="_blank"
                     rel="noreferrer"
                 >
-                    View this project on
+                    {t("view_on_github")}
                     <GitHubIcon className="w-8 h-8 ml-2" />
                 </a>
             </div>
