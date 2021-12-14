4ea3ad830ab2822e1ca0468e5ab4ccceee72d1ab
Thanayut T <b@thanayut.in.th>
2021-12-14 22:06:39 +0700

    chore(translate): Staticpages

--- modules/staticpages/LoginPage.js
+++ modules/staticpages/LoginPage.js
@@ -1,10 +1,13 @@
 import { useRouter } from "next/router";
 import { signIn, useSession } from "next-auth/client";
+import { useTranslation } from "next-i18next";
 import LoginIcon from "../../icons/LoginIcon";
 
 const LoginPage = () => {
     const router = useRouter();
     const [session, loading] = useSession();
+
+    const { t } = useTranslation(["staticpages", "common"]);
 
     // Redirect if logged in
     if (loading) return null;
@@ -16,17 +19,15 @@ const LoginPage = () => {
         <div className="w-5/6 lg:w-2/3 mt-8 mx-auto">
             <div className="flex justify-center">
                 <h1 className="text-2xl text-green-600 font-bold">
-                    Login to Plearncard
+                    {t("login_to_plearncard")}
                 </h1>
             </div>
             <div className="flex justify-center text-center mt-6">
                 <p className="leading-7">
                     <span className="mr-2 text-green-600 font-bold">
-                        Hello!
+                        {t("hello")}
                     </span>
-                    We use OAuth for authentication and authorization here.{" "}
-                    <br />
-                    Click on the button below to choose your favourite provider.
+                    {t("login_desc_1")} <br /> {t("login_desc_2")}
                 </p>
             </div>
             <div className="flex justify-center my-12">
@@ -34,16 +35,16 @@ const LoginPage = () => {
                     className="flex justify-center items-center px-4 h-10 bg-green-200 text-gray-600 rounded-lg cursor-pointer hover:bg-green-300 hover:shadow-sm"
                     onClick={signIn}
                 >
-                    Proceed to Login
+                    {t("proceed_to_login")}
                     <LoginIcon className="w-5 h-5 ml-1" />
                 </span>
             </div>
             <div className="flex justify-center text-center mt-8">
                 <p>
                     <span className="mr-2 text-green-600 font-bold">
-                        First time at Plearncard?
+                        {t("login_desc_3")}
                     </span>
-                    Just click login and select one of our login provider.
+                    {t("login_desc_4")}
                 </p>
             </div>
         </div>
