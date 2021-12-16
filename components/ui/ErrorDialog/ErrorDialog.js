fc11fce445ec0d70cbb7533c0bbfd30a504297bf
Thanayut T <b@thanayut.in.th>
2021-12-16 11:36:21 +0700

    chore: improve general translation

--- components/ui/ErrorDialog/ErrorDialog.js
+++ components/ui/ErrorDialog/ErrorDialog.js
@@ -1,8 +1,12 @@
+import { useTranslation } from "next-i18next";
+
 const ErrorDialog = (props) => {
+    const { t } = useTranslation("common");
+
     return (
         <div className="flex justify-center mt-6">
             <p className="flex justify-center items-center w-full lg:w-1/2 p-3 text-gray-600 bg-red-100 rounded-lg">
-                <span className="font-bold mr-2">Error:</span>
+                <span className="font-bold mr-2">{t("error")}:</span>
                 {props.msg}
             </p>
         </div>
