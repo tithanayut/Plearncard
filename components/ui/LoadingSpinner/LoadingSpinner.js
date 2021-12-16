4340210632642061ad796b7230c453dc9db0b5ed
Thanayut T <b@thanayut.in.th>
2021-12-16 11:36:21 +0700

    chore: improve general translation

--- components/ui/LoadingSpinner/LoadingSpinner.js
+++ components/ui/LoadingSpinner/LoadingSpinner.js
@@ -1,7 +1,10 @@
+import { useTranslation } from "next-i18next";
 import style from "./LoadingSpinner.module.css";
 
 const LoadingSpinner = () => {
-    return <div className={style.loader}>Loading...</div>;
+    const { t } = useTranslation("common");
+
+    return <div className={style.loader}>{t("loading")}...</div>;
 };
 
 export default LoadingSpinner;
